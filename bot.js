13d92a7574319a0268fea65abae4278e40bd9630
medo67 <45091642+medo67@users.noreply.github.com>
2019-01-24 16:13:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,6 @@ var adminprefix = '$'
 
 client.on("message", message => {
     if (message.content.startsWith("$obc")) {
-                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' ');
   message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
